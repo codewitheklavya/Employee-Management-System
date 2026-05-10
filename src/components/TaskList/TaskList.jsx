5985c66f997336a16e7af7dcb0b521eac1bf2eb2
Eklavyasingh7 <codewitheklavya@gmail.com>
2026-05-10 17:20:10 +0530

    tasklist added

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,8 +3,65 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id='Tasklist' className='h-[55%] overflow-x-auto w-full py- items-center justify-start gap-5 flex-nowrap  mt-10 flex '>
-      <div className='h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl'>
-            
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-red-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
+      </div>
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-yellow-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
+      </div>
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-blue-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
+      </div>
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-green-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
+      </div>
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-pink-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
+      </div>
+      <div className='h-full w-[300px] flex-shrink-0 p-5 bg-gray-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>9 may 2026</h4>
+            </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laborum odit enim nihil obcaecati ex rem cum tenetur voluptates magnam!
+            </p>
       </div>
 
       
